cf5726d7078522a32852b4648f46e8c69bfc0caa
Adi <adityak.nimbalkar@gmail.com>
2022-03-06 22:03:51 +0530

    2_comit

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -1,10 +1,12 @@
 from multiprocessing import context
 from django.shortcuts import render, HttpResponse
 
+# find whatsapp
+
 
 def index(request):
     context = {
-        'variable': "this is sent"
+        'variable': "55LPA"
     }
     return render(request, 'index.html', context)
     # return HttpResponse("this is home page")
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"templates")],
+        'DIRS': [os.path.join(BASE_DIR, "template")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
 ]
-#manually Added
+# manually Added
 
 
 # Default primary key field type
